6acaea20fb179cc8f4949c2c40fadfe312e0e647
Desktop Willy <wkim149@gmail.com>
2017-01-08 08:54:33 -0500

    add issuelist ui & issue page ui

--- backend/build/inplaceWebapp/juke.js
+++ backend/build/inplaceWebapp/juke.js
@@ -5,24 +5,25 @@ angular.module('juke', ['ngCookies', 'ngRoute', 'ngMaterial', 'md.data.table'])
     .service('juke_service', function($cookies, $window, $http, baseurl) {
 	var objs = this;
 	objs.get_token = function () {
-		if (!$cookies.getObject('Juke_Access_Token'))
-		    $window.location.href = '/auth.html';
-	
-		return $cookies.getObject('Juke_Access_Token').access_token;
+	    if (!$cookies.getObject('Juke_Access_Token'))
+		$window.location.href = '/auth.html';
+	    
+	    return $cookies.getObject('Juke_Access_Token').access_token;
 	};
 	objs.git_api = function (url, on_success, params = {}) {
-		/* fetch comments */
-		$http({
-		    method: 'GET',
-		    url: baseurl + url,
-		    headers: {
-			'Authorization': 'token ' + objs.get_token()
-		    },
-		    params: params
-		}).then(on_success, function (res) {
-		    console.log('fail');
-		    console.log(res);
-		});
+	    var req_url = (url.startsWith(baseurl) ? url : baseurl + url);
+	    /* fetch comments */
+	    $http({
+		method: 'GET',
+		url: req_url,
+		headers: {
+		    'Authorization': 'token ' + objs.get_token()
+		},
+		params: params
+	    }).then(on_success, function (res) {
+		console.log('fail'); /* TODO */
+		console.log(res);
+	    });
 	};
 	/* bitwise AND will be done for filtering for issue status */
 	objs.status_codes = { 'Open': 1, 'Closed': 2 };
@@ -33,6 +34,53 @@ angular.module('juke', ['ngCookies', 'ngRoute', 'ngMaterial', 'md.data.table'])
 		return 'Closed';
 	    else
 		return 'N/A';
+	};
+	objs.elaborate_event = function (git_event) {
+	    /* COPIED FROM GitHub API v3 /issues/events/
+
+	      closed
+	      The issue was closed by the actor. When the commit_id is present, it identifies the commit that closed the issue using "closes / fixes #NN" syntax.
+	      reopened
+	      The issue was reopened by the actor.
+	      subscribed
+	      The actor subscribed to receive notifications for an issue.
+	      merged
+	      The issue was merged by the actor. The `commit_id` attribute is the SHA1 of the HEAD commit that was merged.
+	      referenced
+	      The issue was referenced from a commit message. The `commit_id` attribute is the commit SHA1 of where that happened.
+	      mentioned
+	      The actor was @mentioned in an issue body.
+	      assigned
+	      The issue was assigned to the actor.
+	      unassigned
+	      The actor was unassigned from the issue.
+	      labeled
+	      A label was added to the issue.
+	      unlabeled
+	      A label was removed from the issue.
+	      milestoned
+	      The issue was added to a milestone.
+	      demilestoned
+	      The issue was removed from a milestone.
+	      renamed
+	      The issue title was changed.
+	      locked
+	      The issue was locked by the actor.
+	      unlocked
+	      The issue was unlocked by the actor.
+	      head_ref_deleted
+	      The pull request's branch was deleted.
+	      head_ref_restored
+	      The pull request's branch was restored.
+	      review_dismissed
+	      The actor dismissed a review from the pull request.
+	      review_requested
+	      The actor requested review from the subject on this pull request.
+	      review_request_removed
+	      The actor removed the review request for the subject on this pull request.
+	    */
+
+	    return 'yeah, this event happened: ' + git_event.event;
 	};
 	return objs;
     })
@@ -65,100 +113,79 @@ angular.module('juke', ['ngCookies', 'ngRoute', 'ngMaterial', 'md.data.table'])
 	    for (var i = 0; i < $scope.repos.length; i++) {
 		var e = $scope.repos[i];
 		e.juke_link = '#!/' + e.full_name;
-		e.juke_rel_time = true;
 		e.juke_updated = moment(e.updated_at).fromNow();
 		e.juke_updated_at = moment(e.updated_at).calendar();
 	    }
 	});
     })
 
-    .controller('issues', function ($scope, $routeParams, juke_service, $timeout, $q) {
-	$scope.codes = juke_service.status_codes;
+    .directive ('mdBlurOnClose', function () {
+	return {
+	    restrict: 'A',
+	    link: function (scope, elem, attrs) {
+		elem.on('focus', function (event) {
+		    elem.blur();
+		});
+	    }
+	};
+    })
+
+    .controller('issues', function ($scope, $routeParams, juke_service, $timeout, $q) {	
 	$scope.token = juke_service.get_token();
 	$scope.owner = $routeParams.owner;
 	$scope.repo = $routeParams.repo;
 	$scope.orderby = 'title';
 	$scope.showing = [];
 
-	$scope.filters = [];
-	$scope.weedfunc = (a, e) => a & e;
-	$scope.$watchCollection('filters', function (n, o) {
-	    $scope.weeder = n.reduce((a, b) => a + b, 0);
-	});
+	$scope.codes = juke_service.status_codes;
+	$scope.filtercnt = Object.keys($scope.codes).length;
 
+	$scope.all_filters = true;
+	$scope.filters = Object.keys($scope.codes);
+	$scope.weedfunc = function (item) {
+	    return item.juke_status_code & $scope.weeder;
+	};
+	$scope.$watchCollection('filters', function (n, o) {
+	    $scope.weeder = n.reduce((a, b) => a + (b ? $scope.codes[b] : 0), 0);
+	    if (n.length < o.length && $scope.all_filters)
+		$scope.all_filters = false;
+	    else if (n.length === $scope.filtercnt && !$scope.all_filters)
+		$scope.all_filters = true;
+	});
 	$scope.filter_text = function () {
-	    if (0 in $scope.filters)
-		return 'All';
+	    if ($scope.all_filters)
+		return 'Showing all status';
 	    else if ($scope.filters.length > 1)
-		return $scope.filters.length + ' filters';
+		return $scope.filters.length + ' filters applied';
+	    else if ($scope.filters.length == 1)
+		return 'Showing ' + $scope.filters[0];
 	    else
+		return 'Filter by Status';
 	};
-	
 	$scope.toggle_all = function (e) {
-	    if ($scope.filters.length) {
-		$scope.filters.length = 0;
-		e.stopPropagation();
-	    } else {
-		$scope.filters = Object.keys($scope.codes).map((k) => $scope.codes[k]);
-	    }
+	    $scope.all_filters = !$scope.all_filters;
+	    if ($scope.all_filters)
+		$scope.filters = Object.keys($scope.codes);
+	    else
+		$scope.filters = [];
+	    e.stopPropagation();
 	};
-	
+
 	/* fetch issues in a repo */
 	$scope.repopath = '/' + $scope.owner + '/' + $scope.repo;
 	juke_service.git_api('/repos' + $scope.repopath + '/issues', function (res) {
 	    $scope.all_issues = res.data;
 	    for (var i = 0; i< $scope.all_issues.length; i++) {
 		var e = $scope.all_issues[i];
-		e.juke_rel_time = true;
 		e.juke_updated = moment(e.updated_at).fromNow();
 		e.juke_updated_at = moment(e.updated_at).calendar();
 		e.juke_link = '#!' + $scope.repopath + '/' + e.number;
 		e.juke_status = juke_service.status(e);
-		/* & 1 is done to include them when filtered with ALL */
 		e.juke_status_code = $scope.codes[e.juke_status];
-		console.log(e);
 	    }
 	    $scope.showing = $scope.all_issues;
 	}, { 'state': 'all' });
 
-	var deferd = null;
-	var search = function (n) {
-	    $scope.typing -= 1;
-	    if ($scope.typing)
-		return;
-
-	    if (!n.length) {
-		$scope.showing = $scope.all_issues;
-		deferd.resolve();
-		return;
-	    }
-
-	    var searched = [];
-	    var rex = new RegExp(n, 'i');
-	    for (var i = 0; i < $scope.all_issues.length; i++) {
-		var e = $scope.all_issues[i];
-		if (rex.test(e.title))
-		    searched.push(e);
-	    }
-	    $scope.showing = searched;
-	    deferd.resolve();
-	};
-
-	$scope.typing = 0;
-	$scope.$watch('keyword', function (n, o) {
-	    if (n === undefined || !$scope.all_issues.length)
-		return;
-
-	    if (!$scope.typing) {
-		deferd = $q.defer();
-		$scope.attendez = deferd.promise;
-	    }
-
-	    $scope.typing += 1;
-	    $timeout(function () {
-		search(n);
-	    }, 1000);
-	});
     })
 
     .controller('issue', function ($scope, $routeParams, juke_service) {
@@ -167,23 +194,82 @@ angular.module('juke', ['ngCookies', 'ngRoute', 'ngMaterial', 'md.data.table'])
 	$scope.repo = $routeParams.repo;
 	$scope.n = $routeParams.n;
 
-	$scope.assignees = [];
-	$scope.events = [];
-	$scope.comments  = [];
+	$scope.statuses = Object.keys(juke_service.status_codes);
 	
 	/* fetch a issue */
 	$scope.path = '/' + $scope.owner + '/' + $scope.repo + '/issues/' + $scope.n;
 	juke_service.git_api('/repos' + $scope.path, function (res) {
-	    $scope.issue = res.data;
-	    $scope.assignees = res.data.assignees;
+	    var e = res.data;
+	    e.juke_created = moment(e.created_at).fromNow();
+	    e.juke_created_at = moment(e.created_at).calendar();
+	    e.juke_status = juke_service.status(e);
+	    e.juke_event_type = 'comment';
+	    $scope.issue = e;
+
+	    $scope.all_events = [$scope.issue];
+	    /* fetch events */
+	    juke_service.git_api($scope.issue.events_url, function (res) {
+		for (var i = 0; i < res.data.length; i++) {
+		    var e = res.data[i];
+		    e.juke_event_type = 'event';
+		    e.juke_created = moment(e.created_at).fromNow();
+		    e.juke_created_at = moment(e.created_at).calendar();
+		}
+
+		$scope.all_events.push(...res.data);
+	    });
+	    
+	    /* fetch comments */
+	    juke_service.git_api($scope.issue.comments_url, function (res) {
+		for (var i = 0; i < res.data.length; i++) {
+		    var e = res.data[i];
+		    e.juke_event_type = 'comment';
+		    e.juke_edited = (e.created_at !== e.updated_at);
+		    e.juke_created = moment(e.created_at).fromNow();
+		    e.juke_created_at = moment(e.created_at).calendar();
+		}
+		$scope.all_events.push(...res.data);
+	    });
 	});
-	/* fetch events */
-	juke_service.git_api('/repos' + $scope.path + '/events', function (res) {
-	    $scope.events = res.data;
-	});
-	/* fetch comments */
-	juke_service.git_api('/repos' + $scope.path + '/comments', function (res) {
-	    $scope.comments = res.data;
-	});
+    })
+
+    .directive('mdJukeEvent', function (juke_service) {
+	return {
+	    restrict: 'EA',
+	    scope: { data: '=' },
+	    link: function (scope, elem, attrs) {
+		scope.event_desc = juke_service.elaborate_event(scope.data);
+	    },
+	    templateUrl: 'juke_event.html'
+	};
+    })
+
+    .directive('mdJukeComment', function () {
+	return {
+	    restrict: 'EA',
+	    scope: { data: '=' },
+	    link: function (scope, elem, attrs) {
+	    },
+	    templateUrl: 'juke_comment.html'
+	};
+    })
+
+    .directive('showOnHover', function ($compile) {
+	return {
+	    restrict: 'E',
+	    scope: { onHover: '@', onLeave: '@' },
+	    link: function (scope, elem, attrs) {
+		scope.hovering = false;
+		elem.on('mouseenter', function () {
+		    scope.hovering = true;
+		    scope.$apply();
+		});
+		elem.on('mouseleave', function () {
+		    scope.hovering = false;
+		    scope.$apply();
+		});
+	    },
+	    template: '<span>{{ hovering ? onHover : onLeave }}</span>'
+	};
     })
 ;
